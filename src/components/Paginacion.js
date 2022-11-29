90af7067533058e61758daa4360d13ac1558a3a0
ren1803 <111241326+ren1803@users.noreply.github.com>
2022-11-28 22:08:09 -0600

    Update Paginacion.js

--- src/components/Paginacion.js
+++ src/components/Paginacion.js
@@ -3,8 +3,10 @@ import React from "react";
 const Paginacion = props => {
     return (
         <div className="py-3">
+            <center>
             <button onClick={props.paginaAnterior} type="button" className="btn btn-info mr-1 ays" id="buscar3">Previous &larr;</button>
             <button  onClick={props.paginaSiguiente} type="button" className="btn btn-info ays2" id="buscar3">Next &rarr;</button>
+            </center>
         </div>
     )
 }
